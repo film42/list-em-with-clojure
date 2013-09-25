3abd6bd82b8170ba0cdf5e35cacd41dbd05f986b
Garrett Thornburg <film42@gmail.com>
2013-09-24 21:25:05 -0600

    weining myself off File class for a wrapper, but I should probs just write my own later

--- src/list_em/core.clj
+++ src/list_em/core.clj
@@ -1,28 +1,35 @@
 (ns list-em.core
   (:require [clojure.string :only (join split)]
-            [clojure.java.io])
+            [clojure.java.io]
+            [me.raynes.fs :as fs])
   (:gen-class))
+
+;; Helper Functions
+(defn f-children [file]
+  (.listFiles file))
 
 ;; Global vector to hold file string list
 (def master-list (atom []))
 
 ;; Recursively go through each directory
 (defn list-dir [dir-name]
-  (let [file (new java.io.File dir-name)]
-    (if (not (.isDirectory file))
+  (let [file (fs/file dir-name)]
+    (if (not (fs/directory? file))
       ;; Is a file, let's save it
-      (let [file-path (.getAbsolutePath file)]
+      (let [file-path (fs/absolute-path file)]
         (swap! master-list conj [file-path])
         (str file-path))
       ;; Is a directory, let's recurse
-      (let [file-list (.listFiles file)]
+      (let [file-list (f-children file)]
+        (println dir-name)
         ;; Send each child through list-dir and return
         (doseq [child file-list]
-          (list-dir (.getAbsolutePath child)))))))
+          (list-dir (fs/absolute-path child)))))))
 
 ;; Save global vector to specified file
 (defn save-dir-list [file-name]
   (doseq [file @master-list]
+;;    (println (first file))
     (spit file-name (str (first file) "\n") :append true)))
 
 ;; Runner
@@ -32,6 +39,7 @@
       (list-dir directory-path)
       (save-dir-list save-file))
     (catch Exception e
+      (println (str e))
       (println "USAGE: java -jar list-em.jar /path/to/dir /path/to/file.txt")))
   ;; Ensure we don't return anything
   nil)
